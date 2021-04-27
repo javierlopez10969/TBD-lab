135c162950c01cb86087a60d40dc327eb428e7f6
martinormazabal <54651468+martinormazabal@users.noreply.github.com>
2021-04-27 12:47:19 -0400

    corrección de la consulta anterior

--- martin/consulta1.sql
+++ martin/consulta1.sql
@@ -1,4 +1,11 @@
-SELECT public.cliente.id_cliente, public.pago.monto
-FROM public.cliente, public.cliente_vehiculo, public.contrato, public.pago
-WHERE public.cliente.id_cliente = public.cliente_vehiculo.id_cliente AND public.contrato.id_cliente_vehiculo = public.cliente_vehiculo.id_cliente_vehiculo AND public.contrato.id_pago = public.pago.id_pago
-ORDER BY public.pago.monto
+SELECT public.edificio_estacion.id_edificio, public.cliente.nombre, MAX(public.pago.monto)
+FROM public.pago, public.cliente, public.cliente_vehiculo, public.contrato, public.edificio_estacion
+WHERE public.cliente.id_cliente = public.cliente_vehiculo.id_cliente AND public.cliente_vehiculo.id_cliente_vehiculo = public.contrato.id_cliente_vehiculo AND public.contrato.id_pago = public.pago.id_pago AND public.contrato.id_edificio = public.edificio_estacion.id_edificio AND public.pago.monto IN 
+(
+	SELECT MAX(public.pago.monto)
+	FROM public.pago, public.contrato, public.edificio_estacion
+	WHERE public.edificio_estacion.id_edificio = public.contrato.id_edificio AND public.pago.id_pago = public.contrato.id_pago
+	GROUP by public.edificio_estacion.id_edificio
+)
+GROUP by public.edificio_estacion.id_edificio, public.cliente.nombre
+ORDER by public.edificio_estacion.id_edificio;
--- martin/consulta2.sql
+++ martin/consulta2.sql
@@ -1,4 +1,14 @@
-SELECT public.modelo.nombre, public.edificio_estacion.nombre, count(public.edificio_estacion.nombre)
+SELECT public.edificio_estacion.id_edificio, public.modelo.marca, count(public.modelo.id_modelo)
 FROM public.modelo, public.vehiculo, public.cliente_vehiculo, public.contrato, public.edificio_estacion
-WHERE public.modelo.id_modelo = public.vehiculo.id_modelo AND public.vehiculo.id_vehiculo = public.cliente_vehiculo.id_vehiculo AND public.contrato.id_cliente_vehiculo = public.cliente_vehiculo.id_cliente_vehiculo AND public.contrato.id_edificio = public.edificio_estacion.id_edificio
-GROUP BY public.edificio_estacion.nombre, public.modelo.nombre;
+WHERE public.modelo.id_modelo = public.vehiculo.id_modelo AND public.cliente_vehiculo.id_vehiculo = public.vehiculo.id_vehiculo AND public.contrato.id_cliente_vehiculo = public.cliente_vehiculo.id_cliente_vehiculo AND public.contrato.id_edificio = public.edificio_estacion.id_edificio
+GROUP by public.edificio_estacion.id_edificio, public.modelo.marca
+HAVING count(public.modelo.id_modelo) IN
+(
+	SELECT count(public.modelo.id_modelo)
+	FROM public.modelo, public.vehiculo, public.cliente_vehiculo, public.contrato, public.edificio_estacion
+	WHERE public.modelo.id_modelo = public.vehiculo.id_modelo AND public.cliente_vehiculo.id_vehiculo = public.vehiculo.id_vehiculo AND public.contrato.id_cliente_vehiculo = public.cliente_vehiculo.id_cliente_vehiculo AND public.contrato.id_edificio = public.edificio_estacion.id_edificio
+	GROUP by public.edificio_estacion.id_edificio
+)
+ORDER by public.edificio_estacion.id_edificio;
+
+
