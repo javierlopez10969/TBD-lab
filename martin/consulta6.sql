70a42d28dd1b379cc838be5e95386883e299573f
martinormazabal <54651468+martinormazabal@users.noreply.github.com>
2021-04-28 21:04:36 -0400

    Perfeccionado, solo muestra el lugar como único respuesta

--- martin/consulta6.sql
+++ martin/consulta6.sql
@@ -2,12 +2,12 @@ DROP VIEW lugar_disponible;
 
 CREATE VIEW lugar_disponible as (SELECT count(public.lugar.id_lugar) as cantidad
 FROM public.edificio_estacion, public.lugar, public.contrato
-WHERE public.edificio_estacion.id_edificio = public.lugar.id_edificio AND NOT (public.edificio_estacion.id_edificio = public.contrato.id_edificio)
+WHERE public.edificio_estacion.id_edificio = public.lugar.id_edificio AND (public.edificio_estacion.id_edificio = public.contrato.id_edificio)
 GROUP by public.edificio_estacion.nombre);
 
 SELECT public.edificio_estacion.nombre, count(public.lugar.id_lugar) as cantidad
 FROM public.edificio_estacion, public.lugar, public.contrato
-WHERE public.edificio_estacion.id_edificio = public.lugar.id_edificio AND NOT (public.edificio_estacion.id_edificio = public.contrato.id_edificio)
+WHERE public.edificio_estacion.id_edificio = public.lugar.id_edificio AND (public.edificio_estacion.id_edificio = public.contrato.id_edificio)
 GROUP by public.edificio_estacion.nombre
-HAVING count(public.lugar.id_lugar) IN (SELECT MAX(lugar_disponible.cantidad)
+HAVING count(public.lugar.id_lugar) IN (SELECT MIN(lugar_disponible.cantidad)
 FROM lugar_disponible);
